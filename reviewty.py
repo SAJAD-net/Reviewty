8220aa0d759b5706e332af68cd82f69fd8aa1c39
SAJADnet <sajadsajad.sch@gmail.com>
2023-11-12 11:47:55 +0330

    getting the plan of a specific date

--- reviewty.py
+++ reviewty.py
@@ -66,7 +66,7 @@ def get_studied_lessons():
     plan_the_review_dates(studied_lessons)
 
 
-def get_todays_plans():
+def get_todays_plan():
     change_dir()
 
     con = sqlite3.connect('reviewty.db')
@@ -84,6 +84,26 @@ def get_todays_plans():
     print(f"\t\t   Today's plan\n{table}")
 
 
+def get_specific_date_plan():
+    change_dir()
+
+    con = sqlite3.connect('reviewty.db')
+    cur = con.cursor()
+
+    print("- Enter a date to get it's plan, Syntax : Year/Month/Day")
+    sdate = input("- Reviewty: ")
+
+    plans = cur.execute(f"SELECT * FROM reviewty WHERE Dates='{sdate}'")
+
+    table = PrettyTable(['Book', 'Units', 'Lessons', 'Pages', 'Dates'])
+
+    for lesson in plans.fetchall():
+        book, units, lessons, pages, date = lesson
+        table.add_row([book, units, lessons, pages, date])
+
+    print(f"\t\t   {sdate} plan\n{table}")
+
+
 def main():
     print("""
 ██████╗ ███████╗██╗   ██╗██╗███████╗██╗    ██╗████████╗██╗   ██╗
@@ -97,21 +117,25 @@ def main():
         database_initialize()
 
     print("[0]- add a new lesson\n[1]- get today's plan")
-    print("[2]- delete a lesson's plan\n[3]- initialize the database\n\
-[4]- initialize a new database\n[5]- delete the database\n[6]- exit\n")
+    print("[2]- get a specific date's plan\n[3]- delete a lesson's plan\
+        \n[4]- initialize the database\n\
+[5]- initialize a new database\n[6]- delete the database\n[7]- exit\n")
     opt = input("~ Reviewty : ")
 
     if opt == '0':
         get_studied_lessons()
     elif opt == '1':
-        get_todays_plans()
+        get_todays_plan()
     elif opt == '2':
-        pass
+        get_specific_date_plan()
     elif opt == '3':
-        database_initialize()
-    #elif opt == '4':
-        #database_initialize(database_name=dname)
+        pass
+    elif opt == '4':
+        pass
     elif opt == '5':
+        #database_initialize(database_name=dname)
+        pass
+    elif opt == '6':
         os.remove('database/reviewty.db')
     else:
         sys.exit()
